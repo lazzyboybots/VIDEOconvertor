6b88a45a7843b194baa5f8ea40a28251c2f683c9
Lazzy Boy <147271056+lazzyboybots@users.noreply.github.com>
2024-12-05 20:12:46 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -18,5 +18,6 @@ FORCESUB = config("FORCESUB", default=None)
 FORCESUB_UN = config("FORCESUB_UN", default=None)
 ACCESS_CHANNEL = config("ACCESS_CHANNEL", default=None)
 MONGODB_URI = config("MONGODB_URI", default=None)
+PORT = config("PORT", default=8080)
 
 Drone = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
